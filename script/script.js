72e98b44bde8fc29839ff15b8fded919f11f12dc
JasmiDW <117347250+JasmiDW@users.noreply.github.com>
2023-01-30 12:39:02 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function initMap() {
+  const selector = document.getElementById("map")
+  const center = { lat: 48.8589466, lng: 2.2769956 }
+  const options = {
+    center: center,
+    zoom : 8,
+  }
+
+const map = new google.maps.Map(selector, options);
+}
